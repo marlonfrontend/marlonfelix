1c74edd612e653354eef27e0bec973fb611e3b29
Marlon Felix <marlon.felix@vigiadepreco.com.br>
2020-02-03 11:35:17 -0300

    init

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="logo">
+    <img src="~/assets/images/logo.svg" alt="">
+  </div>
+</template>
+<style lang="scss">
+  .logo {
+    position: absolute;
+    img {
+      max-width: 60px;
+    }
+  }
+</style>
---
+++ components/Social.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="social">
+    <a href="#">
+      <img src="~/assets/images/github.svg" alt="">
+    </a>
+    <a href="#">
+      <img src="~/assets/images/linkedin.svg" alt="">
+    </a>
+    <a href="#">
+      <img src="~/assets/images/mail.svg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Social'
+}
+</script>
+<style lang="scss" scoped>
+  .social {
+    display: flex;
+    justify-content: center;
+    a {
+      margin: 0 50px;
+    }
+  }
+</style>
